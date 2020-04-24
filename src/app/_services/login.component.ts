e17b198176ea6dbcfa664afa637807709e41fe6a
Rodrigo_Beltran <ptrroot@gmail.com>
2020-04-23 19:29:12 -0500

    Agregamos el login de carlos

--- src/app/_services/login.component.ts
+++ src/app/_services/login.component.ts
@@ -1,25 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthenticationService {
 
     constructor(private http: HttpClient) {}
 
-    login(is: string, password: string) {
-       console.log("Respuesta IS"+is);
-       console.log("Respuesta Password"+password);
-        //return this.http.post('http://70.37.82.204:9090/login', {   is:"CEPC",password:"carlospa" })
-        //    .pipe(map(msj => {
-        //      console.log("Respuesta"+ JSON.stringify(msj) );
-        //      return msj;
-        //    }));
-
-          const headers = { 'Content-Type': 'application/json', 'Accept': 'application/json', 'Access-Control-Allow-Origin':'*','Access-Control-Allow-Credentials':'true' }
-        this.http.post<any>('http://70.37.82.204:9090/login', { is:"CEPC",password:"carlospa" }).subscribe(data => {
-            console.log( data.msj);
+     login(is: string, password: string){
+          const headers = { 'Content-Type': 'application/json', 'Accept': 'application/json', 'Access-Control-Allow-Origin':'*',
+          'Access-Control-Allow-Credentials':'true','Access-Control-Allow-Headers':true}
+        return new Observable((observer)=>{
+            this.http.post<any>('http://localhost:9090/login', { is,password}).subscribe(data => {
+                observer.next(data.msg);
+            }, (error)=>{
+                alert("Ocurrió un problema al realizar la autenticación");
+                observer.error(error);
+            });
         });
-    }
+        
+     }
 
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthenticationService } from '../_services/login.component';
+//import { PaginationService } from '../pagination.service';
+//import { RespuestasService } from '../respuestas.service';
 
 @Component({
   selector: 'app-login',
@@ -9,39 +11,58 @@ import { AuthenticationService } from '../_services/login.component';
 })
 export class LoginComponent implements OnInit {
 
-      loginForm: FormGroup;
-      submitted = false;
+  loginForm: FormGroup;
+  submitted = false;
+  respuestas: {
+    respuesta1: string
+    , respuesta2: string
+    , respuesta3: string
+    , respuesta4: boolean
+    , respuesta5: boolean
+    , respuesta6: boolean
+  };
 
-        constructor(private formBuilder: FormBuilder,
-                    private authService: AuthenticationService
-                  ) { }
+  constructor(private formBuilder: FormBuilder,
+    private authService: AuthenticationService
+    //                    ,private paginationService:PaginationService
+    //                   ,private respuestasService:RespuestasService
+  ) { }
 
-        ngOnInit(): void {
-          this.loginForm = this.formBuilder.group({
-                    username: ['', Validators.required],
-                    password: ['', Validators.required]
-                });
-        }
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      userIS: ['', Validators.required],
+      userPassword: ['', Validators.required]
+    });
+  }
 
-  // convenience getter for easy access to form fields
+  registration() {
+    this.paginationService.moveToPage(6);
+  }
+
+
+  loginLauncher(isValue: string, passValue: string) {
+    this.authService.login(isValue, passValue).subscribe(result => {
+      if (result == "OK") {
+        this.newLogginSucces();
+      } else {
+        alert('Usuario o contraseña incorrectos, intente nuevamente');
+      }
+
+    });
+  }
+
+
   get f() { return this.loginForm.controls; }
 
+
   onSubmit() {
-        this.submitted = true;
-         // stop here if form is invalid
-         //  if (this.loginForm.invalid) {
-         //      return;
-         //  }
-            this.authService.login(this.f.username.value, this.f.password.value);
-        //    .pipe(first())
-          //  .subscribe(
-            //    data => {
-              //      this.router.navigate([this.returnUrl]);
-                //},
-                //error => {
-                  //  this.error = error;
-                  //  this.loading = false;
-                //});
+    this.submitted = true;
+
+    if (this.f.userIS.status == "INVALID" || this.f.userPassword.status == "INVALID") {
+      return;
     }
+    this.loginLauncher(this.f.userIS.value, this.f.userPassword.value);
+  }
+
 
 }
